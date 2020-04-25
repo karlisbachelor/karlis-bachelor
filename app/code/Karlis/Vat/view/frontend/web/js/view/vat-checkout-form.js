9b8ca1ca432eeb53619f3868e6d15cad137f09d3
karlis.manins <karlis.manins@gmail.com>
2020-04-25 17:36:22 +0300

    Creating base ajax call

--- app/code/Karlis/Vat/view/frontend/web/js/view/vat-checkout-form.js
+++ app/code/Karlis/Vat/view/frontend/web/js/view/vat-checkout-form.js
@@ -1,10 +1,19 @@
 /*global define*/
 define([
     'Magento_Ui/js/form/form',
-    'mage/translate'
-], function (Component, $t) {
+    'mage/translate',
+    'ko',
+    'jquery',
+    'mage/url'
+], function (Component, $t, ko, $, url) {
     'use strict';
+
+    const backendUrl = url.build('vat/index/validateandapply');
+
     return Component.extend({
+
+        isLoading: ko.observable(false),
+        // errorValidationMessage: ko.observable(false),
 
         initialize: function () {
             this._super();
@@ -13,7 +22,8 @@ define([
         },
 
         details: function () {
-            return $t('Enter your VAT number below and click "Submit" to confirm it. If the VAT is correct, we will exclude taxes from your order.');
+            return $t('Enter your VAT number below and click "Submit" to confirm it. ' +
+                'If the VAT is correct, we will exclude taxes from your order.');
         },
 
         /**
@@ -22,17 +32,68 @@ define([
          * This method can have any name.
          */
         onSubmit: function () {
-            // trigger form validation
-            this.source.set('params.invalid', false);
-            this.source.trigger('vatCheckoutForm.data.validate');
+
+            var formData = this.source.get('vatCheckoutForm');
+
+            console.log(formData);
+            console.log(formData.hasOwnProperty('vat_number'));
+            console.log(formData['vat_number']);
 
             // verify that form data is valid
-            if (!this.source.get('params.invalid')) {
-                // data is retrieved from data provider by value of the vatScope property
-                var formData = this.source.get('vatCheckoutForm');
-                // do something with form data
-                console.dir(formData);
+            if (formData.hasOwnProperty('vat_number') && formData['vat_number'] !== "") {
+                // Call ajax
+                this._ajax(backendUrl, {'vat_number': formData['vat_number']}, this._onSubmit);
             }
-        }
+        },
+
+
+        _onSubmit: function (response) {
+            console.log('_onSubmitComplete');
+            console.log(response);
+        },
+
+
+        /**
+         * @param {String} url - ajax url
+         * @param {Object} data - post data for ajax call
+         * @param {Function} callback - callback method to execute after AJAX success
+         */
+        _ajax: function (url, data, callback) {
+            $.ajax({
+                url: url,
+                data: data,
+                type: 'post',
+                dataType: 'json',
+                context: this,
+
+                /** @inheritdoc */
+                beforeSend: function () {
+                    // Show loading message
+                    this.isLoading(true);
+                },
+
+                /** @inheritdoc */
+                complete: function () {
+                    // Show loading message
+                    this.isLoading(false);
+                }
+            }).done(function (response) {
+                var msg;
+
+                if (response.success) {
+                    callback.call(this, response);
+                } else {
+                    msg = response['error_message'];
+
+                    if (msg) {
+                        alert({
+                            content: msg
+                        });
+                    }
+                }
+            }).fail(function (error) {
+                console.log(JSON.stringify(error));
+            });
+        },
     });
 });
